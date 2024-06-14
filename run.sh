3f115dc237695a7484845a207cf1670d583f9ef1
Prathamesh314 <kurvep18@gmail.com>
2024-06-15 01:47:35 +0530

    Added gitignore

--- run.sh
+++ run.sh
@@ -4,19 +4,10 @@ filename="$1"
 input_file="input.txt"
 output_file="output.txt"
 
-echo "Running script with file: $filename"
-echo "Input file: $input_file"
-echo "Output file: $output_file"
-
 if [[ "$filename" == *.cpp ]]; then
-    g++ -o "${filename%.*}" "$filename" && echo "Compilation successful" || { echo "Compilation failed"; exit 1; }
-    echo "Running executable..."
-    ./${filename%.*} <"$input_file" >"$output_file"
-    echo "Execution finished."
+	g++ -o "${filename%.*}" "$filename" && ./${filename%.*} <"$input_file" >"$output_file"
 elif [[ "$filename" == *.py ]]; then
-    echo "Running Python script..."
-    python3 "$filename" <"$input_file" >"$output_file"
-    echo "Execution finished."
+	python3 "$filename" <"$input_file" >"$output_file"
 else
-    echo "Unsupported file format"
+	echo "Unsupported file format"
 fi
